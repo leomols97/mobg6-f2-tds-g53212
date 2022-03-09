c3c9110b5dfad8e9cf77f7a4252ca64b0f3db9ef
leomols97 <leomols@hotmail.com>
2022-03-09 16:24:38 +0100

    Projects optimisations

--- TD02/code/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ TD02/code/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,7 +7,6 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
-import com.example.android.navigation.databinding.FragmentTitleBindingImpl
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
